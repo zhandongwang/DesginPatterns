c41268dbec0a6c1c961d03ddf5ca639250d3f4df
王战东 <wangzhaondong09965@hellobike.com>
2020-04-05 22:27:24 +0800

    Initial Commit

---
+++ DesignPatterns/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DesignPatterns
+//
+//  Created by 王战东 on 2020/4/5.
+//  Copyright © 2020 王战东. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
